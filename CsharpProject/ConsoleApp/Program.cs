6e82ba90e9c0ea26fb14ff8381b8b17a6b6e7559
jakaar <jakaar@ttu.ee>
2020-09-29 18:52:48 +0300

    MenuSys

---
+++ CsharpProject/ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.Design;
+using MenuSystem;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========> TIC-TAC-TOE <===========");
+            var menuD = new Menu(MenuLevel.Level2Plus);
+            menuD.AddMenuItem(new MenuItem("Sub 5", "1", DefaulMenuAction));
+            
+            var menuC = new Menu(MenuLevel.Level2Plus);
+            menuC.AddMenuItem(new MenuItem("Sub 4", "1", menuD.RunMenu));
+            
+            var menuB = new Menu(MenuLevel.Level2Plus);
+            menuB.AddMenuItem(new MenuItem("Sub 3", "1", menuC.RunMenu));
+           
+            
+            var menuA = new Menu(MenuLevel.Level1);
+            menuA.AddMenuItem(new MenuItem("Sub 2", "1", menuB.RunMenu));
+            menuA.AddMenuItem(new MenuItem("testing", "2", DefaulMenuAction));
+            
+            var menu = new Menu(MenuLevel.Level0);
+            menu.AddMenuItem(new MenuItem("Go to submenu 1", "s", menuA.RunMenu));
+            
+            menu.AddMenuItem(new MenuItem("New game human vs human. Pointless.", "1", DefaulMenuAction));
+            
+            menu.AddMenuItem(new MenuItem("New game puny human vs mighty AI", "2", DefaulMenuAction));
+            menu.AddMenuItem(new MenuItem("New game mighty AI vs superior AI", "3", DefaulMenuAction));
+            
+            menu.RunMenu();
+        }
+
+        static string DefaulMenuAction()
+        {
+            Console.WriteLine("Not implemented yet!");
+            return "";
+        }
+    }
+}
---
+++ CsharpProject/MenuSystem/Menu.cs
@@ -0,0 +1,139 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MenuSystem
+{
+    public enum MenuLevel
+    {
+        Level0,
+        Level1,
+        Level2Plus 
+    }
+
+    public class Menu
+    {
+        private Dictionary<string, MenuItem> MenuItems { get; set; } = new Dictionary<string, MenuItem>();
+        private readonly MenuLevel _menuLevel;
+        private readonly string[] reservedActions = new[] {"x", "m", "r"};
+        
+        public Menu(MenuLevel level)
+        {
+            _menuLevel = level;
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            if (item.UserChoice == "")
+            {
+                throw new ArgumentException("Userchoice can't be empty or spaces.");
+            }
+            
+            
+            MenuItems.Add(item.UserChoice, item);
+        }
+        
+
+        public string RunMenu()
+        {
+            var userChoice = "";
+            
+            do
+            {
+                Console.Write("");
+
+                foreach (var menuItem in MenuItems)
+                {
+                    Console.WriteLine(menuItem.Value);
+                }
+
+                switch (_menuLevel)
+                {
+                    case MenuLevel.Level0: 
+                        Console.WriteLine("X) eXit");
+                        break;
+                    case MenuLevel.Level1: 
+                        Console.WriteLine("M) return to Main");
+                        Console.WriteLine("X) eXit");
+                        break;
+                    case MenuLevel.Level2Plus: 
+                        Console.WriteLine("R) Return to previous");
+                        Console.WriteLine("M) return to Main");
+                        Console.WriteLine("X) eXit");
+                        break;
+                    default:
+                        throw new Exception("Unknown menu depth!");
+                }
+                Console.Write(">");
+                
+                userChoice = Console.ReadLine()?.ToLower().Trim() ?? "";
+                
+                
+               
+                // Is it a reserved keyword
+                if (!reservedActions.Contains(userChoice))
+                {
+                    // No it wasn't, try to find keyword in MenuItems
+                    if (MenuItems.TryGetValue(userChoice, out var userMenuItem))
+                    {
+                        userChoice = userMenuItem.MethodToExecute();
+                    }else
+                    {
+                        Console.WriteLine("I don't have this option!");
+                    }
+                    
+                }
+
+                if (_menuLevel == MenuLevel.Level0 && userChoice == "r")
+                {
+                    Console.WriteLine("I don't have this option!");
+                    userChoice = "";
+                }
+                if (_menuLevel == MenuLevel.Level0 && userChoice == "m")
+                {
+                    Console.WriteLine("I don't have this option!");
+                    userChoice = "";
+                }
+
+                if (_menuLevel == MenuLevel.Level1 && userChoice == "r")
+                {
+                    Console.WriteLine("I don't have this option!");
+                    userChoice = "";
+                }
+                if (userChoice == "x")
+                {
+                    if (_menuLevel == MenuLevel.Level0)
+                    {
+                        Console.WriteLine("Closing down......");
+                    }
+                    break;
+                }
+                
+                if (_menuLevel != MenuLevel.Level0 && userChoice == "m")
+                {
+                    if (_menuLevel == MenuLevel.Level1)
+                    {
+                        userChoice = "";
+                        break;
+                    }
+
+                    if (_menuLevel == MenuLevel.Level2Plus)
+                    {
+                        userChoice = "m";
+                        break;
+                    }
+                }
+                else
+
+                if (_menuLevel == MenuLevel.Level2Plus && userChoice == "r")
+                {
+                    userChoice = "";
+                    break;
+                }
+
+            } while (userChoice != "x" && userChoice !="m" && userChoice !="r");
+
+            return userChoice;
+        }
+    }
+}
